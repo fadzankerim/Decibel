9ec7725b6351fce5328c36e6405994d0fe542bad
fadzankerim <fadzankerim@gmail.com>
2025-03-02 16:51:02 +0100

    correction

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -23,7 +23,7 @@ interface ChatStore {
     setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL = import.meta.env.MODE === 'development' ? 'http://localhost:5005' : '/api'
+const baseURL = import.meta.env.MODE === 'development' ? 'http://localhost:5005' : '/';
 
 const socket = io(baseURL,{
     autoConnect: false, // only connect if user is authenticated
